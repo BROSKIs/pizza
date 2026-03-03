4e04a677cf789cdc07a132b7845705e6365659d9
Pasha <pavloyousee@gmail.com>
2026-03-03 14:59:18 -0800

    ready to pull

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import mysql2 from "mysql2";
+import dotenv from "dotenv";
+
+//Load enviroment variables from .env
+dotenv.config();
 
 const app = express();
 
 const PORT = 3000;
 
 //Set EJS as the view engine
-app.set("view engine", "EJS");
+app.set("view engine", "ejs");
 
 //"Middlewear" that allows express to read
 //form data and store it in req.body
@@ -19,9 +23,11 @@ const orders = [];
 //DATABASE 
 //create a pool bucket of database connections
 const pool = mysql2.createPool({
-    host: '137.184.36.197',
-    user: 'admin',
-    port: 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 }).promise();
 
 
@@ -43,27 +49,36 @@ app.get("/contact-us", (req,res)=>{
     res.render("contact")
 });
 //Submit response
-app.post("/submit-order", (req,res)=>{
+app.post("/submit-order", async (req,res)=>{
     //{"fname":"z","lname":"z","email":"z","method":"delivery","size":"medium","comment":"","Discounts":"Yes"}
-    //create JSON object to store order data
-    const order = {
-        fname: req.body.fname,
-        lname: req.body.lname,
-        email: req.body.email,
-        method: req.body.method,
-        toppings: req.body.toppings ? req.body.toppings: "none",
-        size: req.body.size,
-        comment: req.body.comment,
-        timestamp: new Date()
-    }
+    //create JSON array to store order data
+    const order = req.body;
+
+    const params = [
+        order.fname,
+        order.lname,
+        order.email,
+        order.size,
+        order.method,
+        Array.isArray(order.toppings) ? order.toppings.join(", ") : "none"
+    ]
+
+    const sql = `INSERT INTO orders (fname, lname, email, size, method, toppings) VALUES (?, ?, ?, ?, ?, ?)`;
+
+    const result = await pool.execute(sql, params);
     //push orders
     orders.push(order);
     res.render("confirmation", { order });
 });
 
 //contact us form
-app.get("/admin", (req,res)=>{
-    res.render("admin", { orders });
+app.get("/admin", async (req,res)=>{
+    //Read all orders from database
+    //newest first
+    let sql = "SELECT * FROM orders ORDER BY timestamp DESC";
+    const orders = await pool.query(sql);
+
+    res.render("admin", { orders:orders[0] });
 });
 
 app.listen(PORT, ()=>{
